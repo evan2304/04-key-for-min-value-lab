5e260ddcabef9721320b6fd3567d3f9634e7ea07
Eva Noertoft <evan2304@hstat.org>
2018-11-08 04:06:07 +0000

    missing one test

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,16 @@
 inp = {:blake => 500, :ashley => 20, :adam => 1}
+inp2 = {:blake => 10, :ashley => 50, :adam => 17}
 def key_for_min_value(name_hash)
     if name_hash.empty?
         return nil 
     else 
-        name_hash.collect do |name, numbers|
+        name_hash.each do |name, numbers|
         if numbers == name_hash.values.min
-            puts name
+            return name_hash.keys.min
         end
     end 
     end 
 end
 
-key_for_min_value(inp)
+#key_for_min_value(inp)
+key_for_min_value(inp2)
