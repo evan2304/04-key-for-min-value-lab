dceed328e455d81309d6e847a9f5a0b0b73b4bd6
Eva Noertoft <evan2304@hstat.org>
2018-11-08 03:58:27 +0000

    add methods

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,14 @@
-# Given a hash with numeric values, return the key for the smallest value
-
+inp = {:blake => 500, :ashley => 20, :adam => 1}
 def key_for_min_value(name_hash)
-    
+    if name_hash.empty?
+        return nil 
+    else 
+        name_hash.collect do |name, numbers|
+        if numbers == name_hash.values.min
+            puts name
+        end
+    end 
+    end 
 end
+
+key_for_min_value(inp)
